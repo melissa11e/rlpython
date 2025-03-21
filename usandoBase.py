38ed90751df04e8ab99e0e48338466be08f2c2fe
Melissa Alvares <143049151+melissa11e@users.noreply.github.com>
2025-03-21 06:34:43 -0300

    Update usandoBase.py

--- usandoBase.py
+++ usandoBase.py
@@ -29,14 +29,17 @@ x1_train, x1_test, y_train, y_test = train_test_split(x1, y, test_size=0.33, ran
 
 reg = LinearRegression ().fit(x1_train.values.reshape(-1,1,),y_train)
 y_pred =reg.predict(x1_test.values.reshape(-1,1))
-
 print("\n                                ..  ...   ")
 print(mean_squared_error(y_test,y_pred))
 print(mean_absolute_error(y_test,y_pred))
 
-fig,ax=plt.subplots()
+fig,gf=plt.subplots()
 
-ax.scatter(y_pred,y_test)
+gf.scatter(y_pred,y_test, color='red', label='dados reais')
+gf.set_xlabel("valores previstos")
+gf.set_ylabel("Valores originais")
+gf.set_title("título")
+plt.legend()
 plt.show()   #os valores debaixo são os previstos, os do lado são os originais
 #esse gráfico acima eu não entendi bulhufas, o que o chat me deu abaixo foi melhor.
 
@@ -57,3 +60,4 @@ renda_prevista = reg.predict([[idade_input]])  # Faz a predição
 
 print(f"Para a idade de {idade_input} anos, a renda prevista é: {renda_prevista[0]:.2f}")
 ##Precisa de [0] porque esse retorno é um array. então retornamos só a primeira posição
+
